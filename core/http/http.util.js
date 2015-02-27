3061e48fb4d3e28ad2d1d77ca64b9719e7635a31
Jesse Javier Cogollo <jessejaviercogollo@Alejandros-iMac-2.local>
2015-02-27 13:42:36 -0500

    use jslint in files

--- core/http/http.util.js
+++ core/http/http.util.js
@@ -3,40 +3,40 @@
 var https = require('http');
 var querystring = require('querystring');
 
-exports.httpRequest = function(opt, method, bodyRequest, path, token, cb) {
-  var bodyRequest = querystring.stringify(bodyRequest);
+exports.httpRequest = function (opt, method, bodyRequest, path, token, cb) {
+  bodyRequest = querystring.stringify(bodyRequest);
   var options = {
-      isHttps:opt.isHttps,
-      host: opt.url,
-      port: opt.port,
-      method: method,
-      path: path,
-       headers: {
-       'Authorization': token,
-       'Accept': 'application/vnd.api+json;revision=1.1',
-       'Content-Length': bodyRequest.length,
-       'Content-Type': 'application/x-www-form-urlencoded'
-       }
+    isHttps: opt.isHttps,
+    host: opt.url,
+    port: opt.port,
+    method: method,
+    path: path,
+    headers: {
+      'Authorization': token,
+      'Accept': 'application/vnd.api+json;revision=1.1',
+      'Content-Length': bodyRequest.length,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
   };
 
-  if(options.isHttps){
-      https = require('https');
+  if (options.isHttps) {
+    https = require('https');
   }
 
   var body = "";
-  var request = https.request(options, function(res){
-    res.on('data', function(data) {
+  var request = https.request(options, function (res) {
+    res.on('data', function (data) {
       body += data;
     });
-    res.on('end', function() {
+    res.on('end', function () {
       var pbody = {};
-      if(body !== ""){
+      if (body !== "") {
         pbody = JSON.parse(body);
       }
       return cb(null, pbody);
-    })
-    res.on('error', function(e) {
-      return (e, null);
+    });
+    res.on('error', function (e) {
+      return cb(e, null);
     });
   });
   request.write(bodyRequest);
--- core/middleware/auth.service.js
+++ core/middleware/auth.service.js
@@ -1,11 +1,13 @@
 'use strict';
 
-function isAuthenticatedServer(meToken){
-  return function(req, res, next){
-    if(req.headers.authorization === meToken){
+function isAuthenticatedServer(meToken) {
+  return function (req, res, next) {
+    if (req.headers.authorization === meToken) {
       next();
-    }else{
-      return res.json(401,{'test':'authenticated fail'});
+    } else {
+      return res.json(401, {
+        'test': 'authenticated fail'
+      });
     }
   };
 }
