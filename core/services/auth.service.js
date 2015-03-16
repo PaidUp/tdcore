bda5844957d4a47ec6a74cd106b450a633b7da7b
Ignacio Pascual <ipascual@unexpectedit.com>
2015-03-16 14:37:15 -0500

    httputil fix

--- core/services/auth.service.js
+++ core/services/auth.service.js
@@ -3,98 +3,128 @@
 var httpUtil = require('../http/http.util');
 var config = require('../config/index');
 
-function init(connection){
-  config.app.connection = connection;
+function init(connection) {
+    config.app.connection = connection;
 }
 
 function signup(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/local/signup', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/local/signup', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function logout(tokenUser, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/auth/logout/userId/' + tokenUser, null, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/auth/logout/userId/' + tokenUser, null, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function login(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/local/login', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/local/login', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function facebook(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/facebook/', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/facebook/', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function verifyRequest(userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/auth/verify-request/userId/' + userId, null, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/auth/verify-request/userId/' + userId, null, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function verify(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/verify/userId/' + userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/verify/userId/' + userId, data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function passwordResetRequest(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/password/reset-request', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/password/reset-request', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function passwordReset(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/password/reset', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/password/reset', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function passwordUpdate(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/password/update/userId/' + userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/password/update/userId/' + userId, data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function emailUpdate(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/email/update/userId/' + userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/auth/email/update/userId/' + userId, data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 exports.signup = signup;
--- core/services/user.service.js
+++ core/services/user.service.js
@@ -2,162 +2,214 @@
 
 var httpUtil = require('../http/http.util');
 var config = require('../config/index');
+var urlencode = require('urlencode');
 
-function init(connection){
-  config.app.connection = connection;
+function init(connection) {
+    config.app.connection = connection;
 }
 
 function create(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/create', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/create', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function current(tokenUser, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/current/?token='+tokenUser, null, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/current/?token=' + urlencode(tokenUser), null, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function update(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/update/userId/'+userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/update/userId/' + urlencode(userId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function find(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/find', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/find', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function contactCreate(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/contact/create/userId/'+userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/contact/create/userId/' + urlencode(userId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function contactList(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/contact/list/userId/'+userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/contact/list/userId/' + urlencode(userId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function contactLoad(userId, contactId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/contact/load/userId/'+userId+'/contactId/'+contactId, null, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/contact/load/userId/' + urlencode(userId) + '/contactId/' + urlencode(contactId), null, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function contactUpdate(data, userId, contactId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.PUT, '/user/contact/update/userId/'+userId+'/contactId/'+contactId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.PUT, '/user/contact/update/userId/' + urlencode(userId) + '/contactId/' + urlencode(contactId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function contactDelete(data, userId, contactId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.DELETE, '/user/contact/delete/userId/'+userId+'/contactId/'+contactId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.DELETE, '/user/contact/delete/userId/' + urlencode(userId) + '/contactId/' + urlencode(contactId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function addressCreate(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/address/create/userId/'+userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/address/create/userId/' + urlencode(userId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function addressList(data, userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/address/list/userId/'+userId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/address/list/userId/' + urlencode(userId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function addressLoad(userId, addressId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/address/load/userId/'+userId+'/addressId/'+addressId, null, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/address/load/userId/' + urlencode(userId) + '/addressId/' + urlencode(addressId), null, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function addressUpdate(data, userId, addressId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.PUT, '/user/address/update/userId/'+userId+'/addressId/'+addressId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.PUT, '/user/address/update/userId/' + urlencode(userId) + '/addressId/' + urlencode(addressId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function addressDelete(data, userId, addressId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.DELETE, '/user/address/delete/userId/'+userId+'/addressId/'+addressId, data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.DELETE, '/user/address/delete/userId/' + urlencode(userId) + '/addressId/' + urlencode(addressId), data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function relationCreate(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/relation/create', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/relation/create', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function relationList(userId, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/relation/list/userId/'+userId, null, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/user/relation/list/userId/' + urlencode(userId), null, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 function save(data, cb) {
-  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/save', data, function (err, data) {
-    if (err) {
-      return cb(err);
-    }
-    return cb(null, data);
-  });
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/user/save', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
 }
 
 exports.create = create;
