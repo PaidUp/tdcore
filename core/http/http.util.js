b3e3c31e8cec6173daeef285b354275b0b5a025e
Jesse Cogollo <cogollo87@gmail.com>
2015-03-18 15:26:06 -0500

    udpate file httpUtil

--- core/http/http.util.js
+++ core/http/http.util.js
@@ -4,45 +4,57 @@ var https = require('http');
 var querystring = require('querystring');
 
 exports.httpRequest = function (opt, method, path, bodyRequest, cb) {
-  bodyRequest = querystring.stringify(bodyRequest);
-  var options = {
-    urlPrefix : opt.urlPrefix ? opt.urlPrefix : '',
-    isHttps: opt.isHttps,
-    host: opt.host,
-    port: opt.port,
-    method: method,
-    path: opt.urlPrefix ? opt.urlPrefix+path : path,
-    headers: {
-      'Authorization': opt.token,
-      'Content-Length': bodyRequest.length,
-      'Content-Type': 'application/x-www-form-urlencoded'
+    var contentType = '';
+    if((typeof(bodyRequest) == 'object')  && (bodyRequest != null)){ 
+        contentType = 'application/json';
+        bodyRequest = JSON.stringify(bodyRequest);
+    } 
+    else {
+        contentType = 'application/x-www-form-urlencoded';
+        bodyRequest = querystring.stringify(bodyRequest);
     }
-  };
-
-  if (options.isHttps) {
-    https = require('https');
-  }
-
-  var body = "";
-  var request = https.request(options, function (res) {
-    res.on('data', function (data) {
-      body += data;
-    });
-    res.on('end', function () {
-      var pbody = {};
-      if (body !== "") {
-        try {
-          pbody = JSON.parse(body);
-        }catch(e){
-          pbody = {};
+    var options = {
+        urlPrefix : opt.urlPrefix ? opt.urlPrefix : '',
+        isHttps: opt.isHttps,
+        host: opt.host,
+        port: opt.port,
+        method: method,
+        path: opt.urlPrefix ? opt.urlPrefix+path : path,
+        headers: {
+            'Authorization': opt.token,
+            'Content-Length': bodyRequest.length,
+            'Content-Type': contentType
         }
-      }
-      return cb(null, pbody);
+    };
+
+    if (options.isHttps) {
+        https = require('https');
+    }
+
+    var body = "";
+    var request = https.request(options, function (res) {
+        res.on('data', function (data) {
+            //console.log('http util data 0',data);
+            body += data;
+            //console.log('http util body 0 ',body);
+        });
+        res.on('end', function () {
+            var pbody = {};
+            if (body !== "") {
+                try {
+                    //console.log('http util body 1',body);
+                    pbody = JSON.parse(body);
+                    //console.log('http util pbody 1',pbody);
+                }catch(e){
+                    pbody = {};
+                }
+            }
+            return cb(null, {status: res.statusCode, body: pbody});
+        });
+        res.on('error', function (e) {
+            return cb(e, null);
+        });
     });
-    res.on('error', function (e) {
-      return cb(e, null);
-    });
-  });
-  request.write(bodyRequest);
-  request.end();
+    request.write(bodyRequest);
+    request.end();
 };
