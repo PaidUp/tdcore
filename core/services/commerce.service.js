4ca47faf2003f7408a109453aa7e72ff5a2ba963
Ignacio Pascual <ipascual@unexpectedit.com>
2015-03-19 13:11:28 -0500

    service

--- core/services/commerce.service.js
+++ core/services/commerce.service.js
@@ -8,7 +8,10 @@ function init(connection) {
     config.app.connection = connection;
 }
 
-exports.createCart = function(data, cb) {
+// **
+// ** Cart
+// **
+exports.cartCreate = function(data, cb) {
     httpUtil.httpRequest(config.app.connection, config.methods.GET, '/commerce/cart/create', data, function (err, data) {
         if (err) {
             return cb(err);
@@ -19,3 +22,216 @@ exports.createCart = function(data, cb) {
         return cb(null, data.body);
     });
 }
+
+exports.cartAdd = function(cartId, shoppingCartProductEntity, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/commerce/cart/add',
+        {
+            cartId:cartId,
+            shoppingCartProductEntity: shoppingCartProductEntity
+        }, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
+}
+
+exports.cartRemove = function(cartId, shoppingCartProductEntity, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/commerce/cart/remove',
+        {
+            cartId:cartId,
+            shoppingCartProductEntity: shoppingCartProductEntity
+        }, function (err, data) {
+            if (err) {
+                return cb(err);
+            }
+            if (data.status !== 200) {
+                return cb(data.body);
+            }
+            return cb(null, data.body);
+        });
+}
+
+exports.cartList = function(cartId, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/commerce/cart/list/' + urlencode(cartId), {}, function (err, data) {
+            if (err) {
+                return cb(err);
+            }
+            if (data.status !== 200) {
+                return cb(data.body);
+            }
+            return cb(null, data.body);
+        });
+}
+
+exports.cartAddress = function(cartId, address, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/commerce/cart/address',
+        {
+            cartId: cartId,
+            shoppingCartAddressEntity: address
+        }, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
+}
+
+exports.cartUpdateProductPrice = function(cartId, productId, amount, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/commerce/cart/update/price',
+        {
+            cartId: cartId,
+            productId: productId,
+            amount: amount
+        }, function (err, data) {
+            if (err) {
+                return cb(err);
+            }
+            if (data.status !== 200) {
+                return cb(data.body);
+            }
+            return cb(null, data.body);
+        });
+}
+
+exports.cartUpdateCustomer = function(cartId, customer, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/commerce/cart/customer',
+        {
+            cartId: cartId,
+            customer: customer
+        }, function (err, data) {
+            if (err) {
+                return cb(err);
+            }
+            if (data.status !== 200) {
+                return cb(data.body);
+            }
+            return cb(null, data.body);
+        });
+}
+
+exports.cartSetShipping = function(cartId, shipping, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/commerce/cart/shipping',
+        {
+            cartId: cartId,
+            shippingMethod: shipping
+        }, function (err, data) {
+            if (err) {
+                return cb(err);
+            }
+            if (data.status !== 200) {
+                return cb(data.body);
+            }
+            return cb(null, data.body);
+        });
+}
+
+exports.cartSetPayment = function(cartId, paymentData, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/commerce/cart/payment',
+        {
+            cartId: cartId,
+            paymentData: paymentData
+        }, function (err, data) {
+            if (err) {
+                return cb(err);
+            }
+            if (data.status !== 200) {
+                return cb(data.body);
+            }
+            return cb(null, data.body);
+        });
+}
+
+exports.cartPlace = function(cartId, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/commerce/cart/place/' + urlencode(cartId), {}, function (err, data) {
+            if (err) {
+                return cb(err);
+            }
+            if (data.status !== 200) {
+                return cb(data.body);
+            }
+            return cb(null, data.body);
+        });
+}
+
+exports.cartView = function(cartId, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/commerce/cart/view/' + urlencode(cartId), {}, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
+}
+
+exports.cartTotals = function(cartId, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/commerce/cart/totals/' + urlencode(cartId), {}, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
+}
+
+// **
+// ** Catalog
+// **
+exports.catalogCategory = function(categoryId, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/commerce/catalog/category/' + urlencode(categoryId), {}, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
+}
+
+exports.catalogProduct = function(productId, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.GET, '/commerce/catalog/product/' + urlencode(productId), {}, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
+}
+
+// **
+// ** Order
+// **
+/*
+ orderLoad(orderId
+ >>>>> GET /order/load/orderId
+ orderList(filter
+ >>>>> POST /order/list
+
+ orderCommentAdd(orderId, comment, status
+ >>>>> POST /order/comment/create
+ orderHold(orderId
+ >>>>> GET /order/status/{status}
+*/
+
+// **
+// ** Transaction
+// **
+/*
+ transactionCreate(orderId, transactionId, details
+ >>>>> POST /transaction/create
+ transactionList(order.incrementId
+ >>>>> GET /transaction/list/order/{orderId}
+ */
