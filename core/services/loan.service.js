973ecd18ca1392bf865c418b2eb945b3dfca1c67
Ricardo Lara <riclara@gmail.com>
2015-03-19 13:38:18 -0500

    complete loan methods and smoke test

--- core/services/loan.service.js
+++ core/services/loan.service.js
@@ -8,7 +8,79 @@ function init(connection) {
     config.app.connection = connection;
 }
 
-function applicationSimulate(data, cb) {
+function loanSimulate(data, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/loan/simulate', data, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+function getLoan(loanId, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/loan/find/loanId/'+loanId, null, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+function loanApplicationCreate(data, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/loan/application/create', data, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+function loanApplicationState(data, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/loan/application/state', data, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+function loanApplicationSign(data, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/loan/application/sign', data, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+function loanApplicationPayment(data, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/loan/application/payment', data, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+function loanApplicationSimulate(data, cb) {
     httpUtil.httpRequest(config.app.connection, config.methods.POST, '/loan/application/simulate', data, function (err, data) {
         if (err) {
             return cb(err);
@@ -20,6 +92,40 @@ function applicationSimulate(data, cb) {
     });
 }
 
-exports.init = init;
-exports.applicationSimulate = applicationSimulate;
+function loanApplicationContract(data, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.POST, '/loan/application/contract', data, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+function loanApplicationFind(applicationId, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/loan/application/find/applicationId/'+urlencode(applicationId), null, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+    if (data.status !== 200) {
+      return cb(data.body);
+    }
+    return cb(null, data.body);
+  });
+}
+
+module.exports = {
+  init : init,
+  loanSimulate : loanSimulate,
+  getLoan : getLoan,
+  loanApplicationCreate : loanApplicationCreate,
+  loanApplicationState : loanApplicationState,
+  loanApplicationSign : loanApplicationSign,
+  loanApplicationPayment : loanApplicationPayment,
+  loanApplicationSimulate : loanApplicationSimulate,
+  loanApplicationContract : loanApplicationContract,
+  loanApplicationFind : loanApplicationFind
+}
 
