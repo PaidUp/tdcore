902a85e20741ca8823225242d413251b971fd95d
Jesse Cogollo <cogollo87@gmail.com>
2015-06-11 21:19:49 -0500

    add legal info to stripe managed account

--- core/services/payment.service.js
+++ core/services/payment.service.js
@@ -35,6 +35,18 @@ exports.addToSCustomer = function (data, cb) {
     });
 };
 
+exports.addLegalCustomer = function (data, cb) {
+    httpUtil.httpRequest(config.app.connection, config.methods.POST, '/customer/add/legal', data, function (err, data) {
+        if (err) {
+            return cb(err);
+        }
+        if (data.status !== 200) {
+            return cb(data.body);
+        }
+        return cb(null, data.body);
+    });
+};
+
 exports.createCard = function (data, cb) {
     httpUtil.httpRequest(config.app.connection, config.methods.POST, '/card/create', data, function (err, data) {
         if (err) {
