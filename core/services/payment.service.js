16c19ccaf6734fc0364b9d33cd3dc8823bbe8a6b
Jesse Cogollo <cogollo87@gmail.com>
2016-06-22 12:08:11 -0500

    add connect service account

--- core/services/payment.service.js
+++ core/services/payment.service.js
@@ -395,6 +395,18 @@ exports.getBalance = function getBalance (connectAccountId, cb) {
   })
 }
 
+exports.retrieveAccount = function retrieveAccount (connectAccountId, cb) {
+  httpUtil.httpRequest(config.app.connection, config.methods.GET, '/account/' + urlencode(connectAccountId), null, function (err, data) {
+    if (err) {
+      return cb(err)
+    }
+    if (data.status !== 200) {
+      return cb(data.body)
+    }
+    return cb(null, data.body)
+  })
+}
+
 exports.getChargesList = function getChargesList (connectAccountId, cb) {
   httpUtil.httpRequest(config.app.connection, config.methods.GET, '/charge/' + urlencode(connectAccountId), null, function (err, data) {
     if (err) {
